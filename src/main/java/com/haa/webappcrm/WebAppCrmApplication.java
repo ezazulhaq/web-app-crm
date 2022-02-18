08b0926c0cdc127e075ab94c6e1a2a499e6dc154
ezazulhaq <61641801+ezazulhaq@users.noreply.github.com>
2022-02-18 20:30:12 +0530

    first commit

---
+++ src/main/java/com/haa/webappcrm/WebAppCrmApplication.java
@@ -0,0 +1,13 @@
+package com.haa.webappcrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppCrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/haa/webappcrm/WebAppCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haa.webappcrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

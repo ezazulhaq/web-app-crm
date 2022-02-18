f7fc8c4df39c03aafbc2d643f140d25aabd92026
ezazulhaq <61641801+ezazulhaq@users.noreply.github.com>
2022-02-18 20:46:32 +0530

    create customer data

---
+++ sql-scripts/01-customer-tracker.sql
@@ -0,0 +1,29 @@
+--
+-- Table structure for table `customer`
+--
+DROP TABLE IF EXISTS `customer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `customer` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) DEFAULT NULL,
+  `last_name` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE = InnoDB AUTO_INCREMENT = 6 DEFAULT CHARSET = latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+--
+-- Dumping data for table `customer`
+--
+LOCK TABLES `customer` WRITE;
+/*!40000 ALTER TABLE `customer` DISABLE KEYS */;
+INSERT INTO
+  `customer`
+VALUES
+  (1, 'David', 'Adams', 'david@luv2code.com'),
+  (2, 'John', 'Doe', 'john@luv2code.com'),
+  (3, 'Ajay', 'Rao', 'ajay@luv2code.com'),
+  (4, 'Mary', 'Public', 'mary@luv2code.com'),
+  (5, 'Maxwell', 'Dixon', 'max@luv2code.com');
+  /*!40000 ALTER TABLE `customer` ENABLE KEYS */;
+UNLOCK TABLES;

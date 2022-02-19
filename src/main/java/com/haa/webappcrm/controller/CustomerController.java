131ac11e424f3e6cd4970d3530ccb11f84342e88
ezazulhaq <61641801+ezazulhaq@users.noreply.github.com>
2022-02-19 10:22:07 +0530

    implement dao on controller

--- src/main/java/com/haa/webappcrm/controller/CustomerController.java
+++ src/main/java/com/haa/webappcrm/controller/CustomerController.java
@@ -1,5 +1,11 @@
 package com.haa.webappcrm.controller;
 
+import java.util.List;
+
+import com.haa.webappcrm.dao.CustomerDAO;
+import com.haa.webappcrm.entity.Customer;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +14,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/customer")
 public class CustomerController {
 
+    @Autowired
+    CustomerDAO customerDAO;
+
     @RequestMapping("/list")
     public String listCustomer(Model theModel) {
+
+        List<Customer> theCustomers = customerDAO.getCustomers();
+
+        theModel.addAttribute("customers", theCustomers);
+
         return "list-customer";
     }
 }
